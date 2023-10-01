5f9e2f5fdfcf6f51fc8ff8399c7bce8cc11e9d9f
Dr. Vortex <prevett.james@gmail.com>
2023-10-01 15:29:00 -0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { parseArgs } from 'node:util';
 import express from 'express';
 import { handler as ssrHandler } from '../dist/blankstorm.net/dist/server/entry.mjs';
-import { handler as apiHandler } from '../dist/api.blankstorm.net/dist/server/entry.mjs';
+//import { handler as apiHandler } from '../dist/api.blankstorm.net/dist/server/entry.mjs';
 import compression from 'compression';
 
 const { values: flags } = parseArgs({
@@ -10,16 +10,16 @@ const { values: flags } = parseArgs({
 	},
 });
 
-const handlers = new Map();
+/*const handlers = new Map();
 handlers.set('default', ssrHandler);
 handlers.set('blankstorm.net', ssrHandler);
-handlers.set('api.blankstorm.net', apiHandler);
+handlers.set('api.blankstorm.net', apiHandler);*/
 
 const app = express();
 app.use(compression());
 app.use('/', express.static('dist/blankstorm.net/dist/client/'));
 app.use(ssrHandler);
-app.use('/', express.static(`dist/api.blankstorm.net/dist/client/`));
-app.use(apiHandler);
+//app.use('/', express.static(`dist/api.blankstorm.net/dist/client/`));
+//app.use(apiHandler);
 
-app.listen(3000);
+app.listen(80);
